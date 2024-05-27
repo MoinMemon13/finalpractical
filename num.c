bf8bcaeaea7eb12aea297a445d84dfcfe4ec3226
Mohammed Moin H Memon <157345294+MoinMemon13@users.noreply.github.com>
2024-05-27 19:02:48 +0530

    Update num.c

--- num.c
+++ num.c
@@ -3,10 +3,10 @@
 int main() {
     int num1, num2, sum;
     
-    printf("Enter the first number: ");
+    printf("Enter the 1st number: ");
     scanf("%d", &num1);
     
-    printf("Enter the second number: ");
+    printf("Enter the 2nd number: ");
     scanf("%d", &num2);
     
     sum = num1 + num2;
